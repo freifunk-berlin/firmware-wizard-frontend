feac842ec0a39ca15cd0b3712ae1002233adfd1f
André Gaul <andre@gaul.io>
2015-07-28 11:50:19 +0200

    add data-binding

--- index.js
+++ index.js
@@ -4,62 +4,104 @@ var wizard = angular.module('WizardApp', [
 
 wizard.controller('WizardCtrl', [
   '$scope', 'leafletData', '$http', function($scope, leafletData, $http) {
-    $scope.wizard = {
-      internetVpn03: true,
+    var routerMarker = {
+      lat: 52.52080,
+      lng: 13.40942,
+      message: 'Your router',
+      focus: true,
+      draggable: true
     };
-    $scope.map = {
-      markers: {
-        router: {
-          lat: 52.52080,
-          lng: 13.40942,
-          message: 'Your router',
-          focus: true,
-          draggable: true
+
+    $scope.wizard = {
+      router: {
+        password: undefined,
+        passwordVerify: undefined,
+        name: undefined
+      },
+      contact: {
+        name: undefined,
+        email: undefined
+      },
+      location: {
+        marker: routerMarker,
+        street: undefined,
+        postalCode: undefined,
+        city: undefined
+      },
+      internet: {
+        share: false,
+        limitDown: undefined,
+        limitUp: undefined,
+        vpn03: {
+          enabled: true,
+          cert: undefined,
+          key: undefined
         }
       },
-      center: {
-        lat: 52.52080,
-        lng: 13.40942,
-        zoom: 10
+      ip: {
+        v4: [
+          {type: 'Wifi 2.4GHz', ip: undefined},
+          {type: 'Wifi 5GHz', ip: undefined},
+          {type: 'LAN', ip: undefined}
+        ],
+        v6Prefix: undefined,
+        distribute: false,
+        v4ClientSubnet: undefined
+      },
+      wifi: {
+
+      }
+    };
+
+    $scope.state = {
+      map: {
+        markers: {
+          router: routerMarker,
+        },
+        center: {
+          lat: 52.52080,
+          lng: 13.40942,
+          zoom: 10
+        }
       }
     };
 
     $scope.$on('leafletDirectiveMarker.dragend', function(event, args) {
-      $scope.map.markers.router.lat = args.model.lat;
-      $scope.map.markers.router.lng = args.model.lng;
+      $scope.state.map.markers.router.lat = args.model.lat;
+      $scope.state.map.markers.router.lng = args.model.lng;
     });
 
     // copy router name to map marker
-    $scope.$watch('wizard.routerName', function(routerName) {
-      $scope.map.markers.router.message = routerName;
+    $scope.$watch('wizard.router.name', function(name) {
+      $scope.state.map.markers.router.message = name;
     });
 
     // keep map centered on marker
-    $scope.$watch('map.markers.router', function(router) {
-      $scope.map.center.lat = router.lat;
-      $scope.map.center.lng = router.lng;
+    $scope.$watch('wizard.location.marker', function(marker) {
+      $scope.state.map.center.lat = marker.lat;
+      $scope.state.map.center.lng = marker.lng;
 
       // search address
-      $scope.getAddress(router.lat, router.lng);
+      $scope.getAddress(marker.lat, marker.lng);
     }, true);
 
     // set marker to current location
     var mapRegistered = false;
     $scope.getLocation = function() {
-      $scope.map.searchingGeolocation = true;
+      $scope.state.map.searchingGeolocation = true;
       leafletData.getMap().then(function(map) {
         map.locate({setView: true, maxZoom: 16, enableHighAccuracy: true});
         if (!mapRegistered) {
           mapRegistered = true;
           map.on('locationfound', function(e) {
             // set marker position
-            $scope.map.markers.router.lat = e.latitude;
-            $scope.map.markers.router.lng = e.longitude;
+            $scope.state.map.markers.router.lat = e.latitude;
+            $scope.state.map.markers.router.lng = e.longitude;
 
-            $scope.map.searchingGeolocation = false;
+            $scope.state.map.searchingGeolocation = false;
           });
           map.on('locationerror', function onLocationFound(e) {
-            $scope.map.searchingGeolocation = false;
+            $scope.state.map.searchingGeolocation = false;
           });
         }
       });
@@ -86,13 +128,11 @@ wizard.controller('WizardCtrl', [
         var postalCode = _.get(_.find(address, {types: ['street_number']}),
                              'long_name');
 
-        angular.extend($scope.wizard, {
-          routerStreet: street && streetNo ? street + ' ' + streetNo :
-            undefined,
-          routerPostalCode: _.get(_.find(address, {types: ['postal_code']}),
-                                  'long_name'),
-          routerCity: _.get(_.find(address, {types: ['locality']}),
-                                  'long_name')
+        angular.extend($scope.wizard.location, {
+          street: street && streetNo ? street + ' ' + streetNo : undefined,
+          postalCode: _.get(_.find(address, {types: ['postal_code']}),
+                            'long_name'),
+          city: _.get(_.find(address, {types: ['locality']}), 'long_name')
         });
 
       }).error(function(data) {
