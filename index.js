5af99d297f360473c8870dfccebccedf767f0c66
André Gaul <andre@gaul.io>
2015-08-02 19:27:31 +0200

    add checkbox 'register new ip addresses'

--- index.js
+++ index.js
@@ -81,6 +81,10 @@ wizard.controller('WizardCtrl', [
           generate: true
         }
       },
+      ip: {
+        register: true,
+        v4ClientSubnetSize: 27
+      },
       wifi: {
         advanced: false,
         devices: {
@@ -259,6 +263,8 @@ wizard.controller('WizardCtrl', [
       );
     };
 
+    $scope.pow = Math.pow;
+
   }
 ]);
 
@@ -285,6 +291,24 @@ wizard.directive('compareTo', function() {
 wizard.filter('base64encode', function() {
   return function(input) {
     return btoa(input);
+  };
+});
+
+wizard.filter('range', function() {
+  return function(input, a, b) {
+    var res = [];
+    var i;
+    if (a < b) {
+      for (i = a; i < b + 1; i++) {
+        res.push(i);
+      }
+    } else {
+      for (i = a; i > b - 1; i--) {
+        res.push(i);
+      }
+    }
+
+    return res;
   };
 });
 
