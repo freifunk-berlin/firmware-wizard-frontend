2d34249798c9913be8425a5cf6d0f7168743adbf
André Gaul <andre@gaul.io>
2015-10-17 14:07:46 +0200

    add ipv4Address directive for ipv4 validation

--- src/js/directives/index.js
+++ src/js/directives/index.js
@@ -2,4 +2,5 @@
 
 module.exports = function(app) {
   require('./compareTo')(app);
+  require('./ipv4Address')(app);
 };
---
+++ src/js/directives/ipv4Address.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var ip = require('ip');
+
+module.exports = function(app) {
+  // check if the provided value is a valid ipv4 address
+  app.directive('ipv4Address', function() {
+    return {
+      require: 'ngModel',
+      link: function(scope, element, attributes, ngModel) {
+        ngModel.$validators.ipv4Address = function(modelValue) {
+          // we run an additional regular expression against the model
+          // because the 'ip' module does not catch all cases
+          var ipRegex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+          if (!ipRegex.exec(modelValue)) {
+            return false;
+          }
+
+          // parse ip with 'ip' module
+          var parsedIp;
+          try {
+            parsedIp = ip.subnet(modelValue, '255.255.255.255');
+          } catch (e) {
+            return false;
+          }
+          return true;
+        };
+
+      }
+    };
+  });
+};
