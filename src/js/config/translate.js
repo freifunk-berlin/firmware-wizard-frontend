ee3595bf6dc28616007d73c0006d23f7710d8dba
André Gaul <andre@gaul.io>
2015-11-15 14:29:17 +0100

    disable 'de' translation (until texts are more stable)

--- src/js/config/translate.js
+++ src/js/config/translate.js
@@ -2,18 +2,24 @@
 
 module.exports = function(app) {
   app.config(function($translateProvider) {
-    $translateProvider.fallbackLanguage(['en', 'de']);
+    $translateProvider.fallbackLanguage('en');
     $translateProvider.useStaticFilesLoader({
       prefix: 'nls/locale-',
       suffix: '.json'
     });
-    $translateProvider.registerAvailableLanguageKeys(['en', 'de'],
-      {
-        'de_*': 'de',
-        'en_*': 'en',
-        '*': 'en'
-      }
-    );
+
+    /*
+      TODO: re-enable 'de' (and possibly others) when texts are a bit more
+            stable.
+    */
+    $translateProvider.registerAvailableLanguageKeys([
+      'en',
+      //'de'
+    ],{
+      //'de_*': 'de',
+      'en_*': 'en',
+      '*': 'en'
+    });
     $translateProvider.determinePreferredLanguage();
 
     // sanitize translations with $sanitize
