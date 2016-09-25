e80bb98cf52c78bd188610cd3b555f04d12b9cab
André Gaul <andre@gaul.io>
2016-09-25 02:13:43 +0200

    fix master detection (again!)

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ "${TRAVIS_BRANCH}" == "travis-deploy" ]] || echo "not on master branch" && exit 0
+[[ "${TRAVIS_BRANCH}" == "travis-deploy" ]] || (echo "not on master branch" && exit 0)
 
 openssl aes-256-cbc -K $encrypted_cd6bced8534a_key -iv $encrypted_cd6bced8534a_iv -in id_rsa_travis.enc -out id_rsa_travis -d
 chmod 0600 id_rsa_travis
