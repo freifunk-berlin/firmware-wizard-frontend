e915b74ad72aeb1ed3fe0a8f873a00136875b960
André Gaul <andre@gaul.io>
2016-12-31 22:22:21 +0100

    online service: update online state

--- src/services/online.js
+++ src/services/online.js
@@ -2,10 +2,15 @@ import { module } from 'angular';
 
 export default module('app.services.online', [])
   .service('online', class OnlineService {
-    constructor($http) {
+    constructor($http, $window) {
       'ngInject';
       this.$http = $http;
       this.check();
+
+      // update when browser detects online/offline changes
+      const boundCheck = this.check.bind(this);
+      $window.addEventListener('online', boundCheck);
+      $window.addEventListener('offline', boundCheck);
     }
 
     check() {
