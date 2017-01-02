cee0797a409e3e8da8967534e57f4ea1d88dd91f
André Gaul <andre@gaul.io>
2017-01-02 11:33:36 +0100

    wizard: add ui-router state definition

--- src/components/wizard/wizard.js
+++ src/components/wizard/wizard.js
@@ -1,11 +1,21 @@
 import { module } from 'angular';
+import uiRouter from 'angular-ui-router';
 
-export default module('app.components.wizard', []).component('wizard', {
-  controller: class WizardCtrl {
-    constructor(online) {
-      'ngInject';
-      this.online = online;
-    }
-  },
-  template: require('./wizard.html'),
-});
+export default module('app.components.wizard', [uiRouter])
+  .config($stateProvider => {
+    'ngInject';
+    $stateProvider.state({
+      name: 'wizard',
+      component: 'wizard',
+      url: '/wizard',
+    });
+  })
+  .component('wizard', {
+    controller: class WizardCtrl {
+      constructor(online) {
+        'ngInject';
+        this.online = online;
+      }
+    },
+    template: require('./wizard.html'),
+  });
