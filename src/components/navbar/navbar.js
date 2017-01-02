bbb834c95c5a6f1d891de688a3b627d77c54f776
André Gaul <andre@gaul.io>
2017-01-02 11:34:55 +0100

    navbar: add ui-router and less

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,5 +1,7 @@
 import { module } from 'angular';
 
+require('./navbar.less');
+
 export default module('app.components.navbar', []).component('navbar', {
   template: require('./navbar.html'),
 });
