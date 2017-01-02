f728364ff7c4356bd805508565dd0f8a88e682c2
André Gaul <andre@gaul.io>
2017-01-02 13:42:34 +0100

    jsonrpc: fix error handling

--- src/services/jsonrpc.js
+++ src/services/jsonrpc.js
@@ -16,9 +16,16 @@ export default module('app.services.jsonrpc', [])
         params: [session, object, method, args]
       }, {
         timeout: 10000
-      }).then(data => {
+      }).then(response => {
+        const data = response.data;
+        if (!data) {
+          return this.$q.reject(new Error(`JSON RPC Error: no data`));
+        }
         if (data.error) {
           return this.$q.reject(new Error(`JSON RPC Error: ${data.error.message} (code ${data.error.code})`));
+        }
+        if (data.result[0] !== 0) {
+          return this.$q.reject(new Error(`JSON RPC Error: non-zero code ${data.result[0]}`));
         }
         return data.result[1];
       });
