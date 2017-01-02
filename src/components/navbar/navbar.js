cec718230199ed2efb8ff05ba4cd6fb8c36cd884
André Gaul <andre@gaul.io>
2017-01-02 11:47:49 +0100

    navbar: collapse by default on mobile

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,5 +3,10 @@ import { module } from 'angular';
 require('./navbar.less');
 
 export default module('app.components.navbar', []).component('navbar', {
+  controller: class NavbarCtrl {
+    constructor() {
+      this.collapsed = true;
+    }
+  },
   template: require('./navbar.html'),
 });
