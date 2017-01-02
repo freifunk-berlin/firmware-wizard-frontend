8f4b3dac5afc628f5a2dbd746acc95b31229c5c4
André Gaul <andre@gaul.io>
2017-01-02 17:12:41 +0100

    navbar: add connection indicator

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,8 +4,18 @@ require('./navbar.less');
 
 export default module('app.components.navbar', []).component('navbar', {
   controller: class NavbarCtrl {
-    constructor() {
+    constructor($uibModal, session) {
+      'ngInject';
+      this.$uibModal = $uibModal;
+      this.session = session;
+
       this.collapsed = true;
+    }
+
+    showConnectModal() {
+      this.$uibModal.open({
+        component: 'connectModal',
+      });
     }
   },
   template: require('./navbar.html'),
