ce61e75b83760ba7c7494697a8151c95f5ec6f09
André Gaul <andre@gaul.io>
2017-01-03 22:08:36 +0100

    base64encode: use utf8

--- src/filters/base64encode.js
+++ src/filters/base64encode.js
@@ -3,7 +3,10 @@ import { module } from 'angular';
 export default module('app.filters.base64encode', [])
   .filter('base64encode', $window => {
     'ngInject';
-    return function(input) {
-      return $window.btoa(input);
+
+    // for unicode
+    // see https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/btoa
+    return function utoa(input) {
+      return $window.btoa(unescape(encodeURIComponent(input)));
     };
   });
