65addebc832141c1653bf5b5a2c6f92700ac7246
André Gaul <andre@gaul.io>
2017-01-03 14:16:16 +0100

    wizard-router: finish output

--- src/components/wizard-router/wizard-router.js
+++ src/components/wizard-router/wizard-router.js
@@ -7,8 +7,28 @@ export default module('app.components.wizard-router', [])
       onRouterUpdate: '&',
     },
     controller: class WizardRouterCtrl {
-      constructor($scope) {
+      constructor($scope, session) {
         'ngInject';
+        this.session = session;
+        $scope.$watchGroup(
+          [
+            '$ctrl.name',
+            '$ctrl.password',
+            '$ctrl.passwordVerify',
+            '$ctrl.sshkeysEnabled',
+            '$ctrl.sshkeys',
+          ],
+          this.updateRouterOutput.bind(this)
+        );
+      }
+
+      updateRouterOutput() {
+        const router = {
+          name: this.name,
+          password: this.password === this.passwordVerify ? this.password : undefined,
+          sshkeys: this.sshkeysEnabled ? this.sshkeys : undefined,
+        };
+        this.onRouterUpdate({router});
       }
     },
     template: require('./wizard-router.html'),
