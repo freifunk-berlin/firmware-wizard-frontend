4fafd73de21294fef1957b603d9e9c40eb1ac44a
André Gaul <andre@gaul.io>
2017-01-03 23:21:07 +0100

    wizard-router: fix passwordHash loops

--- src/components/wizard-router/wizard-router.js
+++ src/components/wizard-router/wizard-router.js
@@ -11,37 +11,53 @@ export default module('app.components.wizard-router', [])
       constructor($scope, session) {
         'ngInject';
         this.session = session;
+        $scope.$watchCollection('$ctrl.router', this.updateFromInput.bind(this));
         $scope.$watchGroup(
           [
             '$ctrl.name',
-            '$ctrl.newPasswordHash',
+            '$ctrl.passwordHash',
             '$ctrl.sshkeysEnabled',
             '$ctrl.sshkeys',
           ],
           this.updateRouterOutput.bind(this)
         );
+        // update passwordHash
         $scope.$watchGroup(
-          ['$ctrl.password', '$ctrl.passwordVerify'],
+          ['$ctrl.password', '$ctrl.passwordVerify', '$ctrl.router.passwordHash'],
           this.updatePasswordHash.bind(this)
         );
       }
 
       updatePasswordHash() {
         if (this.password !== this.passwordVerify) {
-          this.newPasswordHash = undefined;
+          this.passwordHashed = undefined;
+          this.passwordHash = undefined;
           return;
         }
         if (!this.password) {
-          this.newPasswordHash = this.router && this.router.passwordHash;
+          this.passwordHashed = undefined;
+          this.passwordHash = this.router && this.router.passwordHash;
           return;
         }
-        this.newPasswordHash = crypt(this.password);
+        // don't hash again to prevent endless digest loops
+        if (this.password === this.passwordHashed) return;
+        this.passwordHashed = this.password;
+        this.passwordHash = crypt(this.password);
+
+      }
+
+      updateFromInput(router) {
+        if (!router) return;
+        this.name = router.name;
+        this.passwordHash = router.passwordHash;
+        this.sshkeys = router.sshkeys;
+        this.sshkeysEnabled = router.sshkeys !== undefined;
       }
 
       updateRouterOutput() {
         const router = {
           name: this.name,
-          passwordHash: this.newPasswordHash,
+          passwordHash: this.passwordHash,
           sshkeys: this.sshkeysEnabled ? this.sshkeys : undefined,
         };
         this.onRouterUpdate({router});
