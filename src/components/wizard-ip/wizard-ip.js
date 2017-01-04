5e24fbead32f39eab2372d5582c0acf4ad2b6d3e
André Gaul <andre@gaul.io>
2017-01-04 17:55:19 +0100

    wizard-ip: finish migration

--- src/components/wizard-ip/wizard-ip.js
+++ src/components/wizard-ip/wizard-ip.js
@@ -3,6 +3,7 @@ import { copy, module } from 'angular';
 export default module('app.components.wizard-ip', [])
   .component('wizardIp', {
     bindings: {
+      wirelessDevices: '<',
       ip: '<',
       onUpdate: '&',
     },
@@ -11,6 +12,7 @@ export default module('app.components.wizard-ip', [])
         'ngInject';
 
         this.newIp = {};
+        this.pow = Math.pow;
 
         $scope.$watch('$ctrl.ip', this.updateFromInput.bind(this), true);
         $scope.$watch('$ctrl.newIp', this.updateOutput.bind(this), true);
