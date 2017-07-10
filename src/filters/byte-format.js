027365709cf2cfc3342c229d262c58704b866aaa
Andreas Bräu <andreas.braeu@idealo.de>
2017-07-10 13:24:32 +0200

    worked on pull request comments

--- src/filters/byte-format.js
+++ src/filters/byte-format.js
@@ -2,11 +2,12 @@ import { module } from 'angular';
 
 export default module('app.filters.byte-format', [])
     .filter('byteFormat', () => (bytes, precision) => {
+      if (bytes === undefined) return '-';
+      if (bytes === 0) return '0 MB';
+      if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
       let myPrecision = precision;
       const units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'];
       const number = Math.floor(Math.log(bytes) / Math.log(1024));
-      if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
-      if (bytes === 0) return '0 MB';
       if (typeof myPrecision === 'undefined') myPrecision = 1;
       // eslint-disable-next-line no-restricted-properties
       return `${(bytes / (Math.pow(1024, Math.floor(number)))).toFixed(myPrecision)} ${units[number]}`;
