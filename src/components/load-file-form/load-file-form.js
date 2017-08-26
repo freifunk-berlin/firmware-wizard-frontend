9ecb52abcfc3744cc9f8f205fd78abd14ad588ad
André Gaul <andre@gaul.io>
2017-08-27 01:58:08 +0200

    load-file-form: simplify

--- src/components/load-file-form/load-file-form.js
+++ src/components/load-file-form/load-file-form.js
@@ -12,32 +12,12 @@ export default module('app.components.load-file-form', [])
       constructor($scope) {
         'ngInject';
 
-        this.success = false;
-        this.$scope = $scope;
-        this.$ctrl = $scope.$ctrl;
-
-        $scope.$watch('$ctrl.fileContent', this.updateFromInput.bind(this));
-        $scope.$watch('$ctrl.newFileContent', this.updateOutput.bind(this));
+        $scope.$watch('$ctrl.fileContent', this.update.bind(this));
       }
 
-      updateFromInput(value) {
-        this.success = false;
-        this.newFileContent = value;
-        if (this.newFileContent) {
-          this.success = true;
-        }
+      update() {
+        this.onUpdate({fileContent: this.fileContent});
       }
-
-      updateOutput(newFileContent) {
-        const fileContent = copy(newFileContent);
-        this.onUpdate({fileContent});
-      }
-
-      uploadContent(content) {
-        this.newFileContent = content;
-        this.success = true;
-      }
-
     },
     template: require('./load-file-form.html'),
     transclude: true,
